f5746b793a6f07067bfabdaea8e8475c71062e61
CNOliverZhang <cnoliverzhang@gmail.com>
2021-04-17 02:35:48 +0800

    修复了图片压缩工具将图片压缩到其他格式时压缩失效的问题。

--- src/renderer/pages/settings.vue
+++ src/renderer/pages/settings.vue
@@ -269,6 +269,22 @@ export default {
         {
           title: 'Crypto-JS',
           url: 'https://github.com/brix/crypto-js'
+        },
+        {
+          title: 'imagemin',
+          url: 'https://github.com/imagemin/imagemin'
+        },
+        {
+          title: 'imagemin-mozjpeg',
+          url: 'https://github.com/imagemin/imagemin-mozjpeg'
+        },
+        {
+          title: 'imagemin-pngquant',
+          url: 'https://github.com/imagemin/imagemin-pngquant'
+        },
+        {
+          title: 'imagemin-webp',
+          url: 'https://github.com/imagemin/imagemin-webp'
         }
       ],
       version: null,
